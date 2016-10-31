e30e5ecd03525c421be4b76d0437874635b1fe2c
Wuyx <1139965056@qq.com>
2016-10-31 11:45:36 +0800

    Initial Commit of Library
    Signed-off-by: Wuyx <1139965056@qq.com>

---
+++ Example/Pods/Target Support Files/FBSnapshotTestCase/FBSnapshotTestCase-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "FBSnapshotTestCase.h"
+#import "FBSnapshotTestCasePlatform.h"
+#import "UIImage+Compare.h"
+#import "UIImage+Diff.h"
+#import "UIImage+Snapshot.h"
+#import "FBSnapshotTestCase.h"
+#import "FBSnapshotTestCasePlatform.h"
+#import "FBSnapshotTestController.h"
+
+FOUNDATION_EXPORT double FBSnapshotTestCaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char FBSnapshotTestCaseVersionString[];
+
---
+++ Example/Pods/Target Support Files/WaPodTestLib/WaPodTestLib-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double WaPodTestLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char WaPodTestLibVersionString[];
+
